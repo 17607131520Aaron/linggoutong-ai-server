a099692eaa79f00ac6055ffec49e6e9eca84d6ae
v-huangyafeng3 <v-huangyafeng3@xiaomi.com>
2026-05-01 00:07:07 +0800

    feat: 删除业务代码

--- src/main/java/com/linggoutong/server/module/file/mapper/FileMapper.java
+++
@@ -1,9 +0,0 @@
-package com.linggoutong.server.module.file.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.linggoutong.server.module.file.entity.FileInfo;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface FileMapper extends BaseMapper<FileInfo> {
-}
